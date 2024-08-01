358e9ee39f00b5a28feaebb25095d1369223ceaa
Shreya <shreyathummar71@gmail.com>
2024-08-01 14:00:21 +0200

    initial setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        aclonica: ["Aclonica", "sans-serif"],
+        cardo: ["Cardo", "sans-serif"],
+        KolkerBrush: ["Kolker Brush", "sans-serif"],
+      },
+      colors: {
+        primary: "#2F1409",
+        secondary: "#C4A887",
+      },
+    },
   },
   plugins: [],
 };
